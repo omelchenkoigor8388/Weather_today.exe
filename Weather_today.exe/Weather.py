9a93fb6f8173fe1ad8d5535ddec90402a76117e7
Omelchenko Igor <106494942+omelchenkoigor8388@users.noreply.github.com>
2022-06-11 00:16:41 +0300

    readme.md_1

--- Weather_today.exe/Weather.py
+++ Weather_today.exe/Weather.py
@@ -5,8 +5,8 @@ from pyowm.utils import config
 from pyowm.utils.config import get_default_config
 
 class Weather(QtWidgets.QMainWindow, Weather_form.Ui_MainWindow):
-    config_dict = get_default_config()  # Инициализация get_default_config()
-    config_dict['language'] = 'ru'  # Установка языка
+    config_dict = get_default_config()
+    config_dict['language'] = 'ua'
 
     def __init__(self):
         super(Weather, self).__init__()
@@ -19,27 +19,27 @@ class Weather(QtWidgets.QMainWindow, Weather_form.Ui_MainWindow):
         self.weather(place)
 
     def weather(self, place):
-        owm = OWM('1b0a48e855dbaf376a83372d2afd5244')  # Ваш ключ с сайта open weather map
-        mgr = owm.weather_manager()  # Инициализация owm.weather_manager()
+        owm = OWM('1b0a48e855dbaf376a83372d2afd5244')
+        mgr = owm.weather_manager()
         observation = mgr.weather_at_place(place)
-        # Инициализация mgr.weather_at_place() И передача в качестве параметра туда страну и город
+
         w = observation.weather
 
-        status = w.detailed_status  # Узнаём статус погоды в городе и записываем в переменную status
-        w.wind()  # Узнаем скорость ветра
-        humidity = w.humidity  # Узнаём Влажность и записываем её в переменную humidity
+        status = w.detailed_status
+        w.wind()
+        humidity = w.humidity
         temp = w.temperature('celsius')[
-            'temp']  # Узнаём температуру в градусах по цельсию и записываем в переменную temp
-        print("В місті " + str(place) + " зараз " + str(status) +  # Выводим город и статус погоды в нём
+            'temp']
+        print("В місті " + str(place) + " зараз " + str(status) +
               "\nТемпература " + str(
-            round(temp)) + " градусів за цельсієм" +  # Выводим температуру с округлением в ближайшую сторону
-              "\nВологість складає " + str(humidity) + "%" +  # Выводим влажность в виде строки
-              "\nШвидкість вітру " + str(w.wind()['speed']) + " метрів за секунду")  # Узнаём и выводим скорость ветра
+            round(temp)) + " градусів за цельсієм" +
+              "\nВологість складає " + str(humidity) + "%" +
+              "\nШвидкість вітру " + str(w.wind()['speed']) + " метрів за секунду")
 
-        self.label.setText("В місті " + str(place) + " зараз " + str(status) +  # Выводим город и статус погоды в нём
+        self.label.setText("В місті " + str(place) + " зараз " + str(status) +
               "\nТемпература " + str(
-            round(temp)) + " градусів за цельсієм" +  # Выводим температуру с округлением в ближайшую сторону
-              "\nВологість складвє " + str(humidity) + "%" +  # Выводим влажность в виде строки
+            round(temp)) + " градусів за цельсієм" +
+              "\nВологість складвє " + str(humidity) + "%" +
               "\nШвидкість вітру " + str(w.wind()['speed']) + " метрів за секунду")
 
 
--- Weather_today.exe/readme.md
+++ Weather_today.exe/readme.md
@@ -1,1 +1,31 @@
-### 1
+# Weather today v1.2
+
+### What is the program for?
+
+**Weather today v1.2** is an updated version of the program
+previously published on this GitHub account
+[Weather today v1.0](https://github.com/omelchenkoigor8388/Weather_today)
+
+### What changes have been added to the updated version?
+
+- The program received a new format **Weatger.exe**(This makes it 
+easier for new users to launch the program)
+- The program has received an updated icon in the thematic style
+- Removed a difficult procedure for the user with the 
+installation of a unique API key in the middle of the program files
+
+### How to run the program?
+In the updated version, the program received 
+a new format and thus made it easier to run. 
+To do this, download the 
+[**Weather.exe**](https://github.com/omelchenkoigor8388/Weather_today.exe/tree/master/Weather_today.exe/dist) 
+file to your PC and open it as a normal file / program.
+
+![img.png](img.png)
+![img_2.png](img_2.png)
+
+### What was used to create the Weather today program?
+
+- Files from Weather today v1.0
+- **Pyinstaller** library for converting .py files to .exe
+- Adobe Photoshop for making icons
